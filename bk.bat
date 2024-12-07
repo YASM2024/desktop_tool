6320bfb2f865bc2e7617d6a1e2799f6773b37c8b
YASM2024 <ymiyazaki713@gmail.com>
2024-12-08 07:39:33 +0900

    ファイル単体の圧縮に対応、バックアップディレクトリが存在するかのバリデーションを追加

--- bk.bat
+++ bk.bat
@@ -18,7 +18,11 @@ if "%~1"=="" (
   pause
   exit /b
 )
-
+if not exist %backupDir% (
+  echo folder doesnt exist
+  pause
+  exit /b
+) 
 set "currentDir=%~1"
 echo currentDir:%currentDir%
 setlocal enabledelayedexpansion
@@ -29,12 +33,29 @@ echo %basename%
 set "datetime=%date:~0,4%%date:~5,2%%date:~8,2%%time:~0,2%%time:~3,2%%time:~6,2%"
 set "datetime=%datetime: =0%"
 set "archiveName=%basename%_backup_%datetime%.zip"
-%lhaplusPath% /c:zip /p:%password% /n:%backupDir%%archiveName% %currentDir%\*
+echo %archiveName%
+set "backupDirNoQuotes=%backupDir:~1,-1%"
 
-echo backup complete: %backupDir%%archiveName%
+if exist "%currentDir%\" (
+    %lhaplusPath% /c:zip /p:%password% /n:"%backupDirNoQuotes%\%archiveName%" "%currentDir%\*"
+) else (
+    if exist "%currentDir%" (
+        %lhaplusPath% /c:zip /p:%password% /n:"%backupDirNoQuotes%\%archiveName%" "%currentDir%"
+    ) else (
+        echo folder doesnt exist
+        pause
+        exit /b
+    )
+)
+
+if exist "%backupDirNoQuotes%\%archiveName%" (
+    echo backup complete: "%backupDirNoQuotes%\%archiveName%"
+) else (
+    echo backup failed. pause exit /b
+)
 echo do you want to open backup folder?(y/n)
 set /p isopen=
 if /i "%isopen%"=="y" (
-    start "" "%backupDir%"
+    start "" "%backupDirNoQuotes%"
 )
 pause
